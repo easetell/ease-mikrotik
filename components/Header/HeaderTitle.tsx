17b357b221fd4167a2b99d7fd3e700e5b1fbaef5
easetell <109558276+easetell@users.noreply.github.com>
2025-02-19 18:40:45 +0300

    Update HeaderTitle.tsx

--- components/Header/HeaderTitle.tsx
+++ components/Header/HeaderTitle.tsx
@@ -53,7 +53,7 @@ const HeaderTitle = () => {
   return (
     <div className="flex rounded-lg bg-gray-800 p-4 text-white shadow-lg">
       <div className="flex items-center space-x-6">
-        <h1 className="text-2xl font-bold">{mikrotikInfo?.identity}</h1>
+        <h5 className="text-2xl font-bold">{mikrotikInfo?.identity}</h5>
       </div>
     </div>
   );
