61fa950da2b2ce182eaa35fba96dee8d5d682944
easetell <109558276+easetell@users.noreply.github.com>
2025-02-23 22:06:42 +0300

    Update voucherSchema.ts

--- models/voucherSchema.ts
+++ models/voucherSchema.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const voucherSchema = new mongoose.Schema({
   name: { type: String, required: true }, // Username (e.g., "EASETELL")
-  password: { type: String, required: true }, // Password (voucher)
+  password: { type: String, required: true, unique: true }, // Password (voucher) - Unique
   phoneNumber: { type: String, required: true }, // Phone number associated with the voucher
   status: { type: String, enum: ["Unused", "Used"], default: "Unused" }, // Voucher status
   createdAt: { type: Date, default: Date.now }, // Timestamp of creation
