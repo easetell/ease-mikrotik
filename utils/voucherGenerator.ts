e3e4a52d3853184d9742365b2de1a07d3288d88d
easetell <109558276+easetell@users.noreply.github.com>
2025-02-23 21:35:32 +0300

    Update voucherGenerator.ts

--- utils/voucherGenerator.ts
+++ utils/voucherGenerator.ts
@@ -1,7 +1,11 @@
-export function generateVoucher(length = 6) {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  return Array.from(
-    { length },
-    () => chars[Math.floor(Math.random() * chars.length)],
-  ).join("");
-}
+// utils/voucherGenerator.js
+const generateVoucher = () => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let voucherCode = "";
+  for (let i = 0; i < 8; i++) {
+    voucherCode += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return voucherCode;
+};
+
+export default generateVoucher;
