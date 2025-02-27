34a8433bb40c561c242e8ccb000908574116ef7f
Easetell <109558276+easetell@users.noreply.github.com>
2025-02-27 03:20:39 +0300

    cxcx

--- app/api/vouchers/route.ts
+++ app/api/vouchers/route.ts
@@ -5,10 +5,13 @@ import Voucher from "@/models/voucherSchema";
 
 // GET route to fetch all
 export async function GET() {
+  console.log("API hit: Connecting to DB...");
   await connectDB();
+  console.log("DB Connected!");
 
   try {
     const vouchers = await Voucher.find({});
+    console.log("Vouchers fetched:", vouchers.length);
     return NextResponse.json({ vouchers }, { status: 200 });
   } catch (error) {
     console.error("Failed to fetch vouchers:", error);
